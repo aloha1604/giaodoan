e466769d71f4c60634875ce352cb0c92fc2f6a7c
quynam <quynamle123@gmail.com>
2019-10-30 23:08:26 +0700

    fix cap nhat hoa don

--- shopbanhanglaravel/resources/views/pages/lienhe/lienhe.blade.php
+++ shopbanhanglaravel/resources/views/pages/lienhe/lienhe.blade.php
@@ -188,6 +188,9 @@
     			<div class="contact-info" >
 	    				<h2 class="title text-center">Contact Info</h2>
 	    				<p>{{$lienhe->lienhe_noidung}}</p>
+                        <br>
+                        website : TikiStore.com
+                        <br>
                         <a href="{{URL::to($lienhe->lienhe_link)}}">{{$lienhe->lienhe_link}}</a>
 	    						
                         
@@ -195,7 +198,7 @@
 	    		<div class="col-sm-12">    			   			
 					<h2 class="title text-center">Contact <strong>Us</strong></h2>    			    				    				
 					<div id="gmap" class="contact-map">
-						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.9548943308982!2d106.6757144505147!3d10.737959892310109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752fad13fbdfd9%3A0x31e4d76059405939!2zxJDhuqFpIGjhu41jIFRyb3kgKEhvYSBL4buzKQ!5e0!3m2!1svi!2s!4v1571553773849!5m2!1svi!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.120335131211!2d106.71239465051528!3d10.802094592266348!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317528a459cb43ab%3A0x6c3d29d370b52a7e!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBIdXRlY2g!5e0!3m2!1svi!2s!4v1572353653036!5m2!1svi!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 					</div>
 				</div>			 		
 			</div>      			
